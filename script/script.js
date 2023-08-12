e5f8be33ed0349b47b21554bca956145c7d2b5ec
DoBleh <ilhamdile05@gmail.com>
2023-08-12 15:40:24 +0700

    first commit

---
+++ script/script.js
@@ -0,0 +1,59 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let searchForm = document.querySelector(".search-from");
+  let searchBtn = document.querySelector("#search-btn");
+  let shoppingCart = document.querySelector(".shopping-cart");
+  let cartBtn = document.querySelector("#cart-btn");
+  let loginForm = document.querySelector(".login-form");
+  let loginBtn = document.querySelector("#login-btn");
+  let navbar = document.querySelector(".navbar");
+  let menuBtn = document.querySelector("#menu-btn");
+
+  searchBtn.addEventListener("click", () => {
+    searchForm.classList.toggle("active");
+    shoppingCart.classList.remove("active");
+    loginForm.classList.remove("active");
+    navbar.classList.remove("active");
+  });
+
+  cartBtn.addEventListener("click", () => {
+    shoppingCart.classList.toggle("active");
+    searchForm.classList.remove("active");
+    loginForm.classList.remove("active");
+    navbar.classList.remove("active");  
+  });
+
+  loginBtn.addEventListener("click", () => {
+    loginForm.classList.toggle("active");
+    searchForm.classList.remove("active");
+    shoppingCart.classList.remove("active");
+    navbar.classList.remove("active");
+  });
+
+  menuBtn.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+    searchForm.classList.remove("active");
+    shoppingCart.classList.remove("active");
+    loginForm.classList.remove("active");
+  });
+
+  window.onscroll = () => {
+    searchForm.classList.remove("active");
+    shoppingCart.classList.remove("active");
+    loginForm.classList.remove("active");
+    navbar.classList.remove("active");
+  };
+});
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+function toggleButton() {
+  var btn = document.querySelector(".btn");
+  if (btn.classList.contains("active")) {
+    btn.classList.remove("active");
+  } else {
+    btn.classList.add("active");
+  }
+}
